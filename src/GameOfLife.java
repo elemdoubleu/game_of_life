c41d77b945a00c8a9138c9efd188955fb7da88c7
Lisa White <elemdoubleu@gmail.com>
2014-05-26 12:08:49 -0700

    GameOfLife print statements updated

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -3,6 +3,7 @@ public class GameOfLife {
 	
 	public static void main(String[] args) {
 
+		// Setup example GameBoard
 		PieceState[][] currentGameBoard = 
 			{
 					{PieceState.DEAD, PieceState.ALIVE, PieceState.DEAD, PieceState.DEAD, PieceState.DEAD},	
@@ -11,17 +12,21 @@ public class GameOfLife {
 					{PieceState.DEAD, PieceState.ALIVE, PieceState.DEAD, PieceState.DEAD, PieceState.DEAD},	
 					{PieceState.ALIVE, PieceState.DEAD, PieceState.DEAD, PieceState.DEAD, PieceState.ALIVE}
 			};
+		
+		// Print first generation
 		GameBoard currentBoard = new GameBoard(currentGameBoard);	
+		System.out.println("Generation 1:");
 		currentBoard.print();
 		System.out.println();
 		
-		GameBoard SecondGenerationBoard = currentBoard.nextGeneration();
-		SecondGenerationBoard.print();
-		System.out.println();
-		
-		GameBoard ThirdGenerationBoard = SecondGenerationBoard.nextGeneration();
-		ThirdGenerationBoard.print();
-		System.out.println();
+		// Print generations 2 to 5
+		for (int i=2; i<=5; i++)
+		{
+			currentBoard = currentBoard.nextGeneration();
+			System.out.println("\nGeneration " + i + ":");
+			currentBoard.print();
+			System.out.println();
+		}
 		
 	}
 
